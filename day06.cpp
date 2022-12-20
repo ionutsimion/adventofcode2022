fde7a4e93dc263180b8c9956192b1bc78d68d94a
Simion, Ionut <ionut.simion@siemens.com>
2022-12-20 16:03:36 +0200

    Day 6, Part 2 done. There is room for improvement there

--- day06.cpp
+++ day06.cpp
@@ -4,6 +4,19 @@
 
 #include "file.hpp"
 
+namespace
+{
+    bool is_sequence_unique(std::string::const_iterator const &from, long long const sequence_length)
+    {
+        std::string sequence(from, from + sequence_length);
+        std::ranges::sort(sequence);
+        if (auto [one_past_last_unique, one_past_last] = std::ranges::unique(sequence); one_past_last_unique == one_past_last)
+            return true;
+
+        return false;
+    }
+}
+
 namespace aoc
 {
     void day06_tuning_trouble()
@@ -14,28 +27,31 @@ namespace aoc
 
         std::cout << "Day 6:" << std::endl;
 
-        size_t number_of_processed_characters{};
+        size_t number_of_processed_characters_for_marker{};
+        size_t number_of_processed_characters_for_message{};
         try
         {
             std::string line;
             std::getline(*input_stream, line);
-            for (auto from = line.begin(), up_to = std::next(from, 4); up_to != line.end(); from = std::next(from), up_to = std::next(up_to))
-            {
-                std::string group(from, up_to);
-                std::ranges::sort(group);
-                if (auto [one_past_last_unique, one_past_last] = std::ranges::unique(group); one_past_last_unique == one_past_last)
+            for (auto from = line.cbegin(); std::next(from, 4ll) != line.cend(); ++from)
+                if (is_sequence_unique(from, 4ll))
                 {
-                    number_of_processed_characters = std::distance(line.begin(), up_to);
+                    number_of_processed_characters_for_marker = std::distance(line.cbegin(), from) + 4ll;
                     break;
                 }
-            }
+            for (auto from = line.cbegin(); std::next(from, 14ll) != line.cend(); ++from)
+                if (is_sequence_unique(from, 14ll))
+                {
+                    number_of_processed_characters_for_message = std::distance(line.cbegin(), from) + 14ll;
+                    break;
+                }
         }
         catch (std::exception const &e)
         {
             std::cerr << "Exception caught: " << e.what() << std::endl;
         }
 
-        std::cout << "\tPart 1) There were " << number_of_processed_characters << " processed characters" << std::endl;
-        std::cout << "\tPart 2)" << std::endl;
+        std::cout << "\tPart 1) There were " << number_of_processed_characters_for_marker << " processed characters until the first marker was found" << std::endl;
+        std::cout << "\tPart 2) There were " << number_of_processed_characters_for_message << " processed characters until the first message was found" << std::endl;
     }
 }
